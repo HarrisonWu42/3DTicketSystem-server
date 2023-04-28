9ba790951c38d72e4f1ed4e78f5822a5f2e39d26
HarrisonWu42 <harrisonwu.com@gmail.com>
2023-04-28 16:22:43 +0800

    TIC-78 TIC-79 TIC-80

--- server/blueprints/etc.py
+++ server/blueprints/etc.py
@@ -62,7 +62,7 @@ def edit():
 
     etc = Etc.query.get(id)
     if etc is None:
-        return jsonify(code=400, message="Etc not exist.")
+        return jsonify(code=403, message="Etc not exist.")
 
     etc.name = name
     etc.description = description
@@ -121,7 +121,7 @@ def query_etc_info(etc_id):
                                                        'update_timestamp': etc.update_timestamp})
 
 
-# 查询场次列表（根据时间）
+# 查询所有场次列表
 @etc_bp.route('/query_all/<offset>/<page_size>', methods=['GET'])
 def query_all(offset, page_size):
     offset = int(offset)
@@ -131,7 +131,7 @@ def query_all(offset, page_size):
     total_pages = ceil(len(etcs) / page_size)
     data = etcs2json(page_etcs)
     data['total_pages'] = total_pages
-    data['seat_num'] = len(page_etcs)
+    data['etc_num'] = len(page_etcs)
 
     return jsonify(code=200, data=data)
 
@@ -139,14 +139,9 @@ def query_all(offset, page_size):
 # 搜索场次（根据名字）
 @etc_bp.route('/search_by_name/<etc_name>', methods=['GET'])
 def search_by_name(etc_name):
-    etc = Etc.query.filter_by(name=etc_name).first()
-    if etc is None:
+    etcs = Etc.query.filter_by(name=etc_name).all()
+    if etcs is None:
         return jsonify(code=403, message='Etc not exist.')
-
-    return jsonify(code=200, message='Success.', data={'id': etc.id,
-                                                       'name': etc.name,
-                                                       'description': etc.description,
-                                                       'begin_timestamp': etc.begin_timestamp,
-                                                       'end_timestamp': etc.end_timestamp,
-                                                       'create_timestamp': etc.create_timestamp,
-                                                       'update_timestamp': etc.update_timestamp})
+    data = etcs2json(etcs)
+    data['etc_num'] = len(etcs)
+    return jsonify(code=200, data=data)
